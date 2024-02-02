8429e588b00cd918f5aa07c4473d7de1ffbbfc46
Mido-Anwar <mimianwar33@hotmail.com>
2024-02-02 11:07:45 +0200

    for loop lesson

--- array.js
+++ array.js
@@ -3,41 +3,41 @@ console.log(` hello from array file `);
  * array challenge
  */
 console.log(`_______________________ array file __________________________ `);
-// let zero = 0;
-// let counter = 0;
-
-// let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
-// let myfriend = [];
-// //first
-// console.log((myfriend = my.splice(zero, counter + 2).reverse()));
-// switch (zero) {
-//   case 1:
-//     zero = 0;
-//     counter = 2;
-//     myfriend.concat(my.splice(zero, counter).reverse());
-// }
-// // scound
-// console.log(my.slice((zero = 1), (counter = 3)).reverse());
-
-
 
 // Challenge Array
 let zero = 0;
 let counter = 3;
-let my = ["Ahmed","Mazero","Elham","Osama","Gamal","Ameer"];
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
 
 let rever = my.reverse();
-console.log(rever);
+
 console.log(rever.slice(--counter)); // ["Osama","Elham","Mazero","Ahmed"]
 ++counter;
 ++zero;
-console.log(rever);
-console.log(rever.slice(counter,++counter + zero));
-let insert = my.unshift("Elzero");
-console.log(my.slice(0,zero));
 
-let r = my[++counter,counter][--counter];
-let o = my[++counter,counter][counter].toUpperCase();
-let ao = r+o;
+console.log(rever.slice(counter, ++counter + zero));
+let insert = my.unshift("Elzero");
+console.log(my.slice(0, zero));
+
+let r = my[(++counter, counter)][--counter];
+let o = my[(++counter, counter)][counter].toUpperCase();
+let ao = r + o;
 let fo = my.unshift(ao);
-console.log(my.slice(--zero,++zero));
+console.log(my.slice(--zero, ++zero));
+
+//
+let products = ["keybard", "mouse", "pen", "book", "pad", "iphon", "samsung"];
+let colors = ["red", "green", "black"];
+let showCount = 5;
+
+document.write(`<h1>show ${showCount} products</h1>`);
+
+for (let i = 0; i < showCount; i++) {
+  document.write(`<div>`);
+  document.write(`<h3>[${i + 1}] ${products[i]}</h3>`);
+  for (let j = 0; j < colors.length; j++) {
+    document.write(`<p> ${colors[j]}</p>`);
+  }
+  document.write(`<p>${colors.join(" | ")}`);
+  document.write(`</div>`);
+}
